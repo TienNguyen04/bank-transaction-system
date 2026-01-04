8249b578d62a9bd789494b827161a8545d016fec
TienNguyen04 <thuytienng216@gmail.com>
2026-01-04 21:30:45 +0700

    ...

---
+++ transaction-service-Sang/transaction-service/src/main/java/com/example/transaction_service/entity/Account.java
@@ -0,0 +1,22 @@
+package com.example.transaction_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts", schema = "account_service")
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    private BigDecimal balance;
+}
